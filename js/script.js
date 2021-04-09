7d929f3740c872e671902e1302758f0d519a6f4a
DenisKosenko <denys27eu@gmail.com>
2021-04-09 03:39:19 +0300

    beta 2

--- js/script.js
+++ js/script.js
@@ -4,19 +4,18 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
-window.onload = function() {
+request.onload = function() {
+
     const rates = request.response;
     console.log(rates)
 
     rates.forEach(element => {
-      const newItem = document.createElement('tr');
+        const newItem = document.createElement('tr');
+        const values = Object.values(element);
 
-      const values = Object.values(element);
-
-      values.forEach(value => { newItem.innerHTML +=`<td>${value}</td>`});
-      //newItem.innerHTML = `<td>${e.ccy}</td> <td>${e.base_ccy}</td> <td>${e.buy}</td> <td>${e.sale}</td>`;
-      document.querySelector('.rate__table').appendChild(newItem);
-
+        values.forEach(value => { newItem.innerHTML +=`<td>${value}</td>`});
+        //newItem.innerHTML = `<td>${e.ccy}</td> <td>${e.base_ccy}</td> <td>${e.buy}</td> <td>${e.sale}</td>`;
+        document.querySelector('.rate__table').appendChild(newItem);
     });
 }
 
